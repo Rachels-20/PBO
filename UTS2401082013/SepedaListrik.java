f2595e9079894f7531606c44308e2056ea46da40
Rachel Setiawan <akelsetiawan206@gmail.com>
2025-05-20 20:37:10 +0700

    Add files via upload

---
+++ UTS2401082013/SepedaListrik.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UTS2401082013;
+
+/**
+ *
+ * @author akels
+ */
+public class SepedaListrik extends Sepeda{
+    protected int kecepatanMaks;
+    protected int jarakTempuh;
+    
+    public SepedaListrik(){
+        super();
+    }
+    
+    public SepedaListrik(int kecepatanMaks, int jarakTempuh){
+        this.kecepatanMaks=kecepatanMaks;
+        this.jarakTempuh=jarakTempuh;
+    }
+    
+    public void setKecepatanMaks(int kecepatanMaks){
+        this.kecepatanMaks=kecepatanMaks;
+    }
+    
+    public int getKecepatanMaks(){
+        return kecepatanMaks;
+    }
+    
+    public void setJarakTempuh(int jarakTempuh){
+        this.jarakTempuh=jarakTempuh;
+    }
+    
+    public int getJarakTempuh(){
+        return jarakTempuh;
+    }
+}
---
+++ UTS2401082013/Taksi.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UTS2401082013;
+
+/**
+ *
+ * @author akels
+ */
+public class Taksi {
+    
+    protected int tarifAwal;
+    protected int tarifPerKm;
+    
+    public Taksi(){
+        super();
+    }
+    
+    public void setTarifAwal(int tarifAwal){
+        this.tarifAwal=tarifAwal;
+    }
+    
+    public int getTarifAwal(){
+        return tarifAwal;
+    }
+    
+    public void setTarifPerKm(int tarifPerKm){
+        this.tarifPerKm=tarifPerKm;
+    }
+    
+    public int getTarifPerKm(){
+        return tarifPerKm;
+    }
+    
+}
